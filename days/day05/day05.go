e8278edec86a9d9df14f8039220a84ff9db5c99f
Maarten Louage <me@maartenlouage.nl>
2024-12-05 12:50:20 +0100

    Starting day 05

---
+++ days/day05/day05.go
@@ -0,0 +1,13 @@
+package day05
+
+import (
+	"context"
+)
+
+func Part1(ctx context.Context, filename string) (int, error) {
+	return 0, nil
+}
+
+func Part2(ctx context.Context, filename string) (int, error) {
+	return 0, nil
+}
---
+++ days/day05/day05_test.go
@@ -0,0 +1,52 @@
+package day05
+
+import (
+	"context"
+	"testing"
+)
+
+func TestDay05Part1(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+		want     int
+	}{
+		{"sample input", "test_part1.txt", 143},
+		{"real input", "input.txt", 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Part1(context.Background(), tt.filename)
+			if err != nil {
+				t.Fatalf("Part1(%q) error = %v", tt.filename, err)
+			}
+			if got != tt.want {
+				t.Errorf("Part1(%q) = %v, want %v", tt.filename, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDay05Part2(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+		want     int
+	}{
+		{"sample input", "test_part2.txt", 0},
+		{"real input", "input.txt", 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Part2(context.Background(), tt.filename)
+			if err != nil {
+				t.Fatalf("Part1(%q) error = %v", tt.filename, err)
+			}
+			if got != tt.want {
+				t.Errorf("Part1(%q) = %v, want %v", tt.filename, got, tt.want)
+			}
+		})
+	}
+}
